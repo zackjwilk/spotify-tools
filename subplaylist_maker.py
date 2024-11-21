0ab61edc34fc436037be4b54049a1302a267fad9
Zack Wilkinson <44929504+zackjwilk@users.noreply.github.com>
2024-11-21 12:59:08 -0500

    Update subplaylist_maker.py

--- subplaylist_maker.py
+++ subplaylist_maker.py
@@ -59,7 +59,7 @@ load_dotenv()
 # CONSTANTS
 CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
-REDIRECT_URI = "https://myshetland.co.uk/shetland-ponies-in-sweaters/"
+REDIRECT_URI = "https://zackjwilk.github.io/"
 SCOPE = "playlist-read-private user-library-read playlist-modify-private playlist-modify-public"
 
 # Get authorization
